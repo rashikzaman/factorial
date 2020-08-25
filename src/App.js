424eb8540ed1a1d26a4eba7c9bb192576c80d6c7
rashikIcarus <36659537+rashikIcarus@users.noreply.github.com>
2020-08-25 21:42:02 +0600

    factorial functionality added

--- src/App.js
+++ src/App.js
@@ -1,26 +1,48 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from "react";
 
 function App() {
+  const [factorialValue, setFactorialValue] = useState(0);
+  const [inputData, setInputData] = useState(0);
+
+  const processForm = (e) => {
+    e.preventDefault();
+    try {
+      const result = calculateFactorial(inputData);
+      if (result) setFactorialValue(result);
+      else if (result < 1) alert("Invalid Input");
+    } catch (e) {
+      alert(e.message);
+    }
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div>
+        <h1>Factorial Calculator</h1>
+        <form onSubmit={(e) => processForm(e)}>
+          <input
+            type="number"
+            name="factorial"
+            placeholder="Enter a number..."
+            value={inputData}
+            required={true}
+            onChange={(e) => setInputData(e.target.value)}
+          />
+          <br />
+          <button type="submit">Calculate Factorial</button>
+        </form>
+        <h2>Factorial: {factorialValue}</h2>
+      </div>
     </div>
   );
 }
+
+const calculateFactorial = (n) => {
+  if (n < 0) return -1;
+  else if (n === 0) return 1;
+  else {
+    return n * calculateFactorial(n - 1);
+  }
+};
 
 export default App;
